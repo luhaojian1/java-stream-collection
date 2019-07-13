e90357da798655bc1984f1b748f4618fbe690e06
luhaojian11 <962671114@qq.com>
2019-07-13 16:47:38 +0800

    finished test MyMap : should_sort_from_small_to_big

--- src/main/java/com/thoughtworks/collection/MyMap.java
+++ src/main/java/com/thoughtworks/collection/MyMap.java
@@ -22,7 +22,7 @@ public class MyMap {
     }
 
     public List<String> mapLetter() {
-        throw new NotImplementedException();
+        return null;
     }
 
     public List<String> mapLetters() {
@@ -34,6 +34,6 @@ public class MyMap {
     }
 
     public List<Integer> sortFromSmall() {
-        throw new NotImplementedException();
+        return array.stream().sorted().collect(Collectors.toList());
     }
 }
