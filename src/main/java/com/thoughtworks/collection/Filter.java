b7a3fb91935bbc274f066e2227c643de9be5c3f5
luhaojian11 <962671114@qq.com>
2019-07-13 16:04:06 +0800

    finished test filter : should_return_multiple_of_three

--- src/main/java/com/thoughtworks/collection/Filter.java
+++ src/main/java/com/thoughtworks/collection/Filter.java
@@ -19,7 +19,7 @@ public class Filter {
     }
 
     public List<Integer> filterMultipleOfThree() {
-        throw new NotImplementedException();
+        return array.stream().filter(num -> num % 3 == 0).collect(Collectors.toList());
     }
 
     public List<Integer> getCommonElements(List<Integer> firstList, List<Integer> secondList) {
